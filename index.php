edce545072ca501ba4c78db856b644a881c5bb38
Eddybest674 <edidiongeffiong056@gmail.com>
2023-03-22 08:09:03 +0000

    first page to be viewed by a visitor or customer

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Home page</title>
+    <link rel="stylesheet"  href="style.css">
+    <link rel ="stylesheet" href="assets/css/bootstrap.min.css">
+    <script src="assets/js/jquery-3.3.1.min.js"></script>
+	<script src="assets/js/bootstrap.min.js"></script>
+</head>
+<body >
+<marquee style="color: blue;"> Welcome to your online online computer center !!!</marquee> 
+    </div>
+
+
+
+    
+<a href= "login.php"><button type="button" class="btn btn-success btn-lg">Login</button></a>
+<a href= "signup.php"><button type="button" class="btn btn-success btn-lg">sign up</button></a>
+
+<hr class="border border-danger border-2 opacity-50">
+<div class="container text-center">
+  <div class="row g-2">
+    <div class="col-6">
+     <a href="index.php"> <div class="p-3">Tired Of fake Computers ?</div></a>
+    </div>
+    <div class="col-6">
+      <a href="signup.php" ><div class="p-3">Proceed to view our products</div></a>
+    </div>
+  </div>
+</div>
+
+
+
+
+
+</body>
+</html>
